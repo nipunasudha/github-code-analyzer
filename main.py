33038305aee49a0551a0dde8a10bd7d63bd8fcdd
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2020-02-05 20:03:38 +0530

    Implement custom ruleset

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from utils import query_yes_no
-from utils.utils import get_repo_list, delete_dir, clone_by_url
+from utils.utils import get_repo_list, clone_by_url, empty_dir
 
 while True:
-    username = input('Enter username to fetch repositories: ')
-    # username = 'upulrangana'  # todo remove this
+    # username = input('Enter username to fetch repositories: ')
+    username = 'githubanalyzeruser'  # todo remove this
     if username.strip() is '':
         print('Invalid username! Please try again.')
         continue
@@ -14,10 +16,12 @@ while True:
             print(f'No repositories found for user \'{username}\'! Please try again.')
             continue
         proceed = query_yes_no(f'Do you want to clone all {repo_count} repositories found for the user \'{username}\'?')
-        delete_dir('./repos')
+        empty_dir('./repos')
         if proceed:
             for url in repo_list:
                 clone_by_url(url)
         else:
             print('User aborted analysis.')
         break
+
+os.system('pmd -d ./repos/ -R rulesets/java/quickstart.xml,ruleset.xml -f html > output.html')
--- utils/utils.py
+++ utils/utils.py
@@ -16,6 +16,18 @@ def on_delete_error(action, name, exc):
     os.remove(name)
 
 
+def empty_dir(dir_path):
+    for filename in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path, onerror=on_delete_error)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
 def clone_by_url(repo_url):
     p = giturlparse.parse(repo_url)
     print(f'Fetching github repository {p.owner}/{p.repo}...')
