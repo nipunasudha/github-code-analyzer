b79704e259163881c27013a15c257fb703b5386e
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2020-02-14 13:25:23 +0530

    Implement performance scoring

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -2,6 +2,7 @@ import csv
 import json
 import os
 
+from analyzer.counter import count_lines
 from utils import try_numeric
 
 
@@ -31,8 +32,15 @@ def categorize_inspections(unsorted_inspections):
     return inspections
 
 
+def get_performance(line_count):
+    return 1 - (line_count['errorLines']/line_count['codeLines'])
+
+
 def generate_report(csv_path):
     inspections = generate_dictionary(csv_path)
+    line_count = count_lines('./repos', ['.java', '.js'])
+    line_count['errorLines'] = len(inspections)
+    performance_score = get_performance(line_count)
     with open('data.json', 'w') as outfile:
         json.dump(inspections, outfile, sort_keys=True, indent=4,
                   ensure_ascii=False)
@@ -45,3 +53,8 @@ def generate_report(csv_path):
         for ruleKey, ruleVal in rulesetVal.items():
             rule_count += 1
             print(f' > {rule_count}: {ruleKey} ({len(ruleVal)} issues)')
+    print('=' * 20)
+    print(f'Total code lines: {line_count["codeLines"]}')
+    print(f'Total lines with violations: {line_count["errorLines"]}')
+    print(f'Performance Score: {"{0:.2%}".format(performance_score)}')
+    print('=' * 20)
---
+++ analyzer/counter.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import codecs
+import os.path
+
+
+def count_lines(path, extensions):
+    comment_symbol = "//"
+    if not extensions:
+        print('Please pass at least one file extension as an argument.')
+        quit()
+    files_to_check = []
+    for root, _, files in os.walk(path):
+        for f in files:
+            fullpath = os.path.join(root, f)
+            if '.git' not in fullpath:
+                for extension in extensions:
+                    if fullpath.endswith(extension):
+                        files_to_check.append(fullpath)
+    if not files_to_check:
+        print('No files found.')
+        quit()
+    line_count = 0
+    total_blank_line_count = 0
+    total_comment_line_count = 0
+    for fileToCheck in files_to_check:
+        with codecs.open(fileToCheck, encoding='utf-8', errors='ignore') as f:
+
+            file_line_count = 0
+            file_blank_line_count = 0
+            file_comment_line_count = 0
+
+            for line in f:
+                line_count += 1
+                file_line_count += 1
+
+                line_without_whitespace = line.strip()
+                if not line_without_whitespace:
+                    total_blank_line_count += 1
+                    file_blank_line_count += 1
+                elif line_without_whitespace.startswith(comment_symbol):
+                    total_comment_line_count += 1
+                    file_comment_line_count += 1
+    total_code_line_count = line_count - total_blank_line_count - total_comment_line_count
+    return {
+        'blankLines': total_blank_line_count,
+        'codeLines': total_code_line_count,
+    }
