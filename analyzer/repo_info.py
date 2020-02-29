d1db737bb3c80edd6a651d8b6c8513839219506f
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2020-02-29 11:11:29 +0530

    Fix cloning related bugs

--- analyzer/repo_info.py
+++ analyzer/repo_info.py
@@ -1,4 +1,5 @@
-from github import Github
+from github import Github, GithubException
+
 
 ########################################
 # for fetching meta information about repositories
@@ -16,13 +17,16 @@ class RepoInfoFetcher(object):
         total_commits = 0
         languages = {}
         for repo in repos:
-            langs = repo.get_languages()
-            for lang, chars in langs.items():
-                if lang in languages:
-                    languages[lang] += chars
-                else:
-                    languages[lang] = chars
-            total_commits += repo.get_commits().totalCount
+            try:
+                langs = repo.get_languages()
+                for lang, chars in langs.items():
+                    if lang in languages:
+                        languages[lang] += chars
+                    else:
+                        languages[lang] = chars
+                total_commits += repo.get_commits().totalCount
+            except GithubException as exception:
+                continue
 
         print(f'Username: {username}')
         print(f'Repositories: {repo_count}\nCommits: {total_commits}')
